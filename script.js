b2fccddc65565d5cb551dcd1d0460328224f59c1
Abbos <abbos.pankiy2018@gmail.com>
2024-03-19 14:31:27 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// hometask //
+//  let arr = [ 25,55,71,23,94];
+
+//  console.log(arr.sort((a, b) => a - b)); (birin- ketinlikda)
+//  console.log(arr.sort((a, b) => b - a));  (teskari holatda)
+//  console.log(arr.sort((a,b) => a - b) .slice(-1)); (eng maksimal soni ciqarib beradi)
+// console.log(arr.sort((a, b) => a - b)[0]); (eng minimalani holatni chiqarib beradi)
+// console.log(arr.sort((a, b) => b - a)[0]); (eng maxsimum soni ciqarib beradi)
+
+///////////////////////////////////////////////////////
+// let cars = [
+//     { id: 1, year: 1998, engine: 1, name: 'Tico'},
+//     { id: 2, year: 2005, engine: 1.2, name: 'Matiz'},
+//     { id: 3, year: 2010, engine: 1.6, name: 'Gentra'},
+//     { id: 4, year: 2010, engine: 1.5, name: 'Cobalt'},
+//     { id: 5, year: 2012, engine: 2, name: 'Malibu'},
+//     { id: 6, year: 2000, engine: 1.2, name: 'Damas'},
+//     { id: 7, year: 2018, engine: 2.4, name: 'Tracker'}
+// ];
+// let filter = cars.filter((value) => value.year < 2000);
+// console.log(filter);
+
+// let filter = cars.filter((value)=>value.year >2010 );
+// console.log(filter);
+
+// let sorting = cars.sort((a, b) => a.engine - b.engine);
+// console.log(sorting);
+
+// let sorting = cars.sort((a, b) => a.year - b.year);
+// console.log(sorting);
+
+// let sorting = cars.sort((a, b) => a.name.localeCompare(b.name));
+// console.log(sorting);
+
+
+// let filtering = cars.filter((value) => value.year < 2000);
+// let updating = filtering.map((value) => {
+//     return {...value, status: 'eski'}
+// })
+// console.log(updating);
+
+
+//  let filtering = cars.filter((value) => value.year >= 2000 && value.year <= 2010);
+// let updating = filtering.map((value) => {
+//     return {...value, status: 'orta'}
+// })
+// console.log(updating);
+
